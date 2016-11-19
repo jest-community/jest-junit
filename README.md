0934de011ac6c78dd16d50d0f104760dfbdbb73f
Jason Palmer <jason@jason-palmer.com>
2016-11-18 20:48:52 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # jest-junit
 A Jest testResultsProcessor that creates compatible junit xml files
+
+## Installation
+yarn add --dev jest-junit
+
+## Usage
+In your jest config add the following entry:
+```JSON
+{
+  "testResultsProcessor": "./node_modules/jest-junit"
+}
+```
+
+Then simply run:
+
+```shell
+jest
+```
+
+## Configuration
+
+jest-junit offers two configurations based on environment variables.
+
+```JEST_SUITE_NAME```: Default "jest tests"
+
+```JEST_JUNIT_OUTPUT```: Default "./junit.xml"
+
+Example:
+
+```shell
+JEST_SUITE_NAME="Jest JUnit Unit Tests" JEST_JUNIT_OUTPUT="./artifacts/junit.xml" jest
+```
+
+Example output:
+```xml
+<testsuites name="Jest JUnit Unit Tests">
+  <testsuite name="My first suite" tests="1" errors="0" failures="0" skipped="0" timestamp="2016-11-19T01:37:20" time="0.105">
+    <testcase classname="My test case" name="My test case" time="6">
+    </testcase>
+  </testsuite>
+</testsuites>
+```
---
+++ index.js
@@ -0,0 +1,98 @@
+'use strict';
+
+const xml = require('xml');
+const jsyaml = require('js-yaml');
+const mkdirp = require('mkdirp');
+const fs = require('fs');
+const path = require('path');
+
+const SUITE_NAME = process.env.JEST_SUITE_NAME || 'jest tests';
+const OUTPUT_PATH = process.env.JEST_JUNIT_OUTPUT ||
+                    path.join(process.cwd(), './junit.xml');
+
+/*
+  At the end of ALL of the test suites this method is called
+  It's responsible for generating a single junit.xml file which
+  Represents the status of the test runs
+
+  Expected input and workflow documentation here:
+  https://facebook.github.io/jest/docs/configuration.html#testresultsprocessor-string
+
+  Intended output (junit XML) documentation here:
+  http://help.catchsoftware.com/display/ET/JUnit+Format
+*/
+module.exports = (report) => {
+  // Generate a single XML file for all jest tests
+  let jsonResults = {
+    'testsuites': [
+      {
+        '_attr': {
+          'name': SUITE_NAME
+        }
+      }
+    ]
+  };
+
+  // Iterate through outer testResults (test suites)
+  report.testResults.forEach((suite) => {
+    // Add <testsuite /> properties
+    let testSuite = {
+      'testsuite': [{
+        _attr: {
+          name: suite.testResults[0].ancestorTitles[0],
+          tests: suite.numFailingTests + suite.numPassingTests + suite.numPendingTests,
+          errors: 0,  // not supported
+          failures: suite.numFailingTests,
+          skipped: suite.numPendingTests,
+          timestamp: (new Date(suite.perfStats.start)).toISOString().slice(0, -5),
+          time: (suite.perfStats.end - suite.perfStats.start) / 1000
+        }
+      }]
+    };
+
+    // Iterate through test cases
+    suite.testResults.forEach((tc) => {
+      let testCase = {
+        'testcase': [{
+          _attr: {
+            classname: tc.fullName,
+            name: tc.fullName,
+            time: tc.duration
+          }
+        }]
+      };
+
+      // Write out all failure messages as <failure> tags
+      // Nested underneath <testcase> tag
+      if (tc.status === 'failed') {
+        tc.failureMessages.forEach((failure) => {
+          testCase.testcase.push({
+            'failure': [{
+              _attr: {
+                message: failure || ''
+              }
+            }]
+          });
+        })
+      }
+
+      // Write out a <skipped> tag if test is skipped
+      // Nested underneath <testcase> tag
+      if (tc.status === 'pending') {
+        testCase.testcase.push({
+          skipped: {}
+        });
+      }
+
+      testSuite.testsuite.push(testCase);
+    });
+
+    jsonResults.testsuites.push(testSuite);
+  });
+
+  // Ensure output path exists
+  mkdirp.sync(path.dirname(OUTPUT_PATH));
+
+  // Write data to file
+  fs.writeFileSync(OUTPUT_PATH, xml(jsonResults, { indent: '  '}));
+};
