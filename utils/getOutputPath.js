f7df8d92fb5cc562228b5866eaf00cf79ed5a18f
Andreas Hässler <andreas.haessler@outlook.at>
2022-10-15 17:25:58 +0200

    Add boolean support for uniqueOutputName option

--- utils/getOutputPath.js
+++ utils/getOutputPath.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const getOptions = require('./getOptions');
 
-module.exports = (options, jestRootDir)  => {
+module.exports = (options, jestRootDir) => {
   // Override outputName and outputDirectory with outputFile if outputFile is defined
   let output = options.outputFile;
   if (!output) {
     // Set output to use new outputDirectory and fallback on original output
-    const outputName = (options.uniqueOutputName === 'true') ? getOptions.getUniqueOutputName(options.outputName) : options.outputName
+    const outputName = (options.uniqueOutputName === 'true' || options.uniqueOutputName === true)
+      ? getOptions.getUniqueOutputName(options.outputName)
+      : options.outputName
     output = getOptions.replaceRootDirInOutput(jestRootDir, options.outputDirectory);
     const finalOutput = path.join(output, outputName);
     return finalOutput;
